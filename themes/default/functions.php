d8539b3b9e3ad97ae38abb83ad1b98947472bba7
Steve L <59241607+steveldev@users.noreply.github.com>
2022-02-04 14:33:05 +0100

    Update functions.php

--- themes/default/functions.php
+++ themes/default/functions.php
@@ -2,5 +2,5 @@
 add_action( 'wp_enqueue_scripts', 'load_assets' );
 function load_assets() {
     wp_enqueue_style( 'style-name-css', get_stylesheet_uri() );
-    wp_enqueue_script( 'script-name-js', get_template_directory_uri() . '/js/scripts.js', [], false, true );
+    wp_enqueue_script( 'script-name-js', get_template_directory_uri() . '/assets/dist/js/scripts.js', [], false, true );
 }
