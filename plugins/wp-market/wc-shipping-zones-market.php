af8e3d6eedf22c3c0738cf77d7123023bb49b9db
Steve L <59241607+steveldev@users.noreply.github.com>
2022-04-28 09:01:47 +0200

    Add files via upload

--- plugins/wp-market/wc-shipping-zones-market.php
+++ plugins/wp-market/wc-shipping-zones-market.php
@@ -20,76 +20,92 @@ new WCShippingZonesMarket();
 class WCShippingZonesMarket {
 
     public function __construct() {        
+        add_action( 'admin_menu', [$this, 'add_menu_page'] );
         add_filter( 'woocommerce_get_sections_shipping', [$this, 'market_add_section'] );
         add_filter( 'woocommerce_get_settings_shipping', [$this, 'market_all_settings'], 10, 2 );
     }
     
-/**
- * Create the section beneath the products tab
- **/
-public function market_add_section( $sections ) {
+    public function add_menu_page(  ) {
 	
-	$sections['market'] = __( 'Marchés', 'text-domain' );
-	return $sections;
-	
-}
-
-public function market_all_settings( $settings, $current_section ) {
-
-    // get markets
-    $markets = get_posts([
-        'post_type' => 'market',
-        'numberposts'=> 50
-    ]);
-
-    foreach($markets as $market) {
-        $markets_options[$market->ID] = $market->post_title;
+        add_menu_page(
+            __( 'Marchés', 'textdomain' ),
+            'Mes Marchés',
+            'manage_options',
+            'admin.php?page=wc-settings&tab=shipping&section=market',
+            '',
+            'dashicons-networking',
+            2
+        );
+    }
+        
+    /**
+     * Create the section beneath the products tab
+     **/
+    public function market_add_section( $sections ) {
+        
+        $sections['market'] = __( 'Marchés', 'text-domain' );
+        return $sections;
+        
     }
 
+    public function market_all_settings( $settings, $current_section ) {
 
-    // section description
-    $section_description = '<p>Séléctionnez les marchés correspondants à vos points de vente.</p>';
-    $section_description .= '<br>Vous pouvez également ajouter des marchés de notre <a href="">Moteur de recherche</a>';
+        // get markets
+        $markets = get_posts([
+            'post_type' => 'market',
+            'numberposts'=> 50
+        ]);
+
+        foreach($markets as $market) {
+            $markets_options[$market->ID] = $market->post_title;
+        }
 
 
-	/**
-	 * Check the current section is what we want
-	 **/
-	if ( $current_section == 'market' ) {
-		$settings_slider = array();
-		// Add Title to the Settings
-		$settings_slider[] = array( 
-            'name' => __( 'Gestion des Marchés', 'text-domain' ), 
-            'type' => 'title', 
-            'desc' => __( $section_description, 'text-domain' ), 
-            'id' => 'market' 
-        );
-		// Add checkbox option
-		$settings_slider[] = array(
-			'name'     => __( 'Marchés', 'text-domain' ),
-			'desc_tip' => __( 'Information text', 'text-domain' ),
-			'id'       => 'market_auto_insert',
-			'type'     => 'select',
-			'css'      => 'min-width:300px;',
-			'desc'     => __( 'Helper text', 'text-domain' ),
-            'multiple' => true,
-            'options'  => $markets_options,
-		);
+        // section description
+        $section_description = '<p>Séléctionnez les marchés correspondants à vos points de vente.</p>';
+        $section_description .= '<br>Vous pouvez également ajouter des marchés de notre <a href="">Moteur de recherche</a>';
 
-		
-		$settings_slider[] = array( 
-            'type' => 'sectionend', 
-            'id' => 'market' 
-        );
-		return $settings_slider;
-	
-	/**
-	 * If not, return the standard settings
-	 **/
-	} else {
-		return $settings;
-	}
 
-}
+        /**
+         * Check the current section is what we want
+         **/
+        if ( $current_section == 'market' ) {
+            $settings = array();
+
+            // Add Title to the Settings
+            $settings[] = array( 
+                'name' => __( 'Gestion des Marchés', 'text-domain' ), 
+                'type' => 'title', 
+                'desc' => __( $section_description, 'text-domain' ), 
+                'id' => 'market' 
+            );
+            
+            // Add checkbox option
+            $settings[] = array(
+                'name'     => __( 'Marchés', 'text-domain' ),
+                'desc_tip' => __( 'Information text', 'text-domain' ),
+                'id'       => 'market_auto_insert',
+                'type'     => 'select',
+                'css'      => 'min-width:300px;',
+                'desc'     => __( 'Helper text', 'text-domain' ),
+                'multiple' => true,
+                'options'  => $markets_options,
+            );
+
+            
+            $settings[] = array( 
+                'type' => 'sectionend', 
+                'id' => 'market' 
+            );
+            return $settings;
+        
+        /**
+         * If not, return the standard settings
+         **/
+        } else {
+            return $settings;
+        }
+
+    }
 
 }
