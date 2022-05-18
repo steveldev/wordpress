295ad2457b7fc415cce8045ae2897d1f4a4900f2
Steve L <59241607+steveldev@users.noreply.github.com>
2022-05-18 05:49:03 +0200

    Update demo.php

--- plugins/demo/demo.php
+++ plugins/demo/demo.php
@@ -4,8 +4,6 @@
  * Author: Steve LERAT (contact@reseau-net.fr)
  * Author URI: https://reseau-net.fr
  * Version: 1.0
- * Plugin URI: https://dev.reseau-net.fr/wp-json/api/wp-plugins
- * Update URI: http://localhost/wp-json/api/wp-plugins
  *
  */
 ?>
